2fcf2ec287501480d0a31b142dd9e0ce761c9cf1
Blowze <krainov@synergy-lab.ru>
2019-06-12 22:42:25 +0300

    Gathered project , made a frame desktop

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,18 @@
-import jQuery from "jquery";
-
-jQuery(function() {
-  jQuery("body").css("color", "blue");
+$(document).ready(function () {
+    $('.accordion__head').on('click', function(e) {
+        var parent = $(this).parent();
+        $('.accordion__content', parent).slideToggle(parent.is('.accordion__item-collapsed'));
+        parent.toggleClass('accordion__item-collapsed');
+    });
+    //tabs
+    $(".tabs .tabs__item").on('click', function (event) {
+        event.preventDefault();
+        $(".tabs .tabs__item").removeClass("tabs__item_active");
+        $(this).addClass("tabs__item_active");
+        var tab = $(this).attr("href");
+        console.log(tab)
+        $(".tabs  .tabs__content").not(tab).css("display", "none");
+        $(tab).show();
+        return false;
+    });
 });
